971e4d7348b074a3df5de0de0ce407181a9ff95d
Van Ha Nguyen <vanhaxl@gmail.com>
2018-09-29 09:35:07 -0500

    inital project commit

---
+++ src/main/java/vhnguyen/azuretopicsubscription/topsubdemo/TopsubDemoApplication.java
@@ -0,0 +1,12 @@
+package vhnguyen.azuretopicsubscription.topsubdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopsubDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopsubDemoApplication.class, args);
+	}
+}
